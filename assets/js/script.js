61b85d814ce6d02144035583889bb2910f352f17
Jakub Chrobak <jchrobakk@gmail.com>
2022-01-29 16:39:49 +0100

    change inputS

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const stepZero = document.querySelector('.step-0');
+const btnZero = stepZero.querySelector('.box__cta');
+const stepOne = document.querySelector('.step-1');
+
+btnZero.addEventListener('click', () => {
+	stepZero.classList.remove('box--visible');
+	stepOne.classList.add('box--visible');
+});
+
+const form = document.querySelector('form');
+const telephone = document.querySelector('#telephone');
+const code = document.querySelector('#code');
+
+telephone.addEventListener('change', () => {
+	// check if telephone is number and length is 9
+
+	if (!telephone.value.length === 9 || isNaN(telephone.value)) {
+		console.log('please enter a valid telephone number');
+	}
+});
+
+// form.addEventListener('change', (e) => {
+// 	e.preventDefault();
+// 	console.log('change');
+// });
